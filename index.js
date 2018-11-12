e7e8ea4ebaa72e28e21dd604a5c0acf1c6f0b3cf
brunoschubert <brunoschubertm@gmail.com>
2018-11-12 17:44:49 -0200

    Add auto-text clear

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-
-//create new Tasks and add to the list
+//create new todo item
 function addTask() {
     //takes the user input
-    let newUl = document.getElementById("ul");
-    let userInput = document.getElementById("item");
+    let newUl = document.getElementById("incomplete-task");
+    let userInput = document.getElementById("newTask");
     let newItem = document.createElement("li");
     //creates a checkbox and add to li
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.value = 1;
+    checkbox.value = "";
     checkbox.name = "newCheckbox";
+    checkbox.id = "checkbox";
     //create a new li and add user input to it
     newItem.appendChild(checkbox);    
-    newItem.setAttribute('id', userInput.value);
+    newItem.setAttribute('id', "incomplete");
     newItem.appendChild(document.createTextNode(userInput.value));
     //appends the new li do the document
     newUl.appendChild(newItem);
+    //clears newText box
+    document.getElementById("newTask").value = ""; 
 }
 
+function completeTask() {
 
-
+}
 
