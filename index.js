6016b4ae061ee24c9b654d84b9b07cbdd3db62b8
brunoschubert <brunoschubertm@gmail.com>
2018-11-12 16:23:05 -0200

    Add Checkbox to new tasks

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-//define variables for entering new tasks
-/*let userInput = document.getElementById("input");
-let buttonCLick = document.getElementById("button")[0]; 
-let incompleteTasks = document.getElementById("incomplete-task");
-let completeTasks = document.getElementById("complete-task");*/
-//creating new Tasks
+
+//create new Tasks and add to the list
 function addTask() {
+    //takes the user input
     let newUl = document.getElementById("ul");
     let userInput = document.getElementById("item");
     let newItem = document.createElement("li");
+    //creates a checkbox and add to li
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = 1;
+    checkbox.name = "newCheckbox";
+    //create a new li and add user input to it
+    newItem.appendChild(checkbox);    
     newItem.setAttribute('id', userInput.value);
     newItem.appendChild(document.createTextNode(userInput.value));
+    //appends the new li do the document
     newUl.appendChild(newItem);
 }
+
+
 
 
