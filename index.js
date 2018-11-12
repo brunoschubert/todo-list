ae9a23a860db2393101f4ae874eb5e71525a0b4b
brunoschubert <brunoschubertm@gmail.com>
2018-11-12 19:07:59 -0200

    Refactored code for simplicity

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ function addTask() {
     let newUl = document.getElementById("incomplete-task");
     let userInput = document.getElementById("newTask");
     let newItem = document.createElement("li");
-    //creates a checkbox and add to li
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.value = "";
-    checkbox.name = "newCheckbox";
-    checkbox.id = "checkbox";
+    //create new div
+    let div = document.createElement("div");
+    div.setAttribute("class", "task");
     //create a new li and add user input to it
-    newItem.appendChild(checkbox);    
+    newItem.appendChild(div);    
     newItem.setAttribute('id', "incomplete");
     newItem.appendChild(document.createTextNode(userInput.value));
     //appends the new li do the document
     newUl.appendChild(newItem);
     //clears newText box
-    document.getElementById("newTask").value = ""; 
+    document.getElementById("newTask").value =""; 
 }
-
-function completeTask() {
-
-}
-
+//Create method to line-through element
